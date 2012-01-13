80288fbccd5c4849e308256c1410c44de689d745
itziakos <ioannist@enthought.com>
2012-01-13 23:54:29 +0000

    Refactor out the FunctionDoc class

--- function_doc.py
+++ function_doc.py
@@ -1,40 +1,13 @@
 #------------------------------------------------------------------------------
-#  file: function.py
+#  file: function_doc.py
 #  License: LICENSE.TXT
 #
 #  Copyright (c) 2011, Enthought, Inc.
 #  All rights reserved.
 #------------------------------------------------------------------------------
-from base_doc import (BaseDoc, get_indent, is_empty, remove_indent,
-                      add_indent, Field)
-
-
-class ArgumentField(Field):
-
-    def rst(self, indent=''):
-        lines = []
-        name = self.name.replace('*','\*')
-        param_str = indent + ':param {0}: {1}'.format(name, self.desc[0].strip())
-        type_str = indent + ':type {0}: {1}'.format(name, self.type)
-        lines.append(param_str)
-        for line in self.desc[1:]:
-            lines.append('{0}'.format(line))
-        if len(self.type) > 0:
-            lines.append(type_str)
-        return lines
-
-class RaisesField(Field):
-
-    def rst(self, indent='', prefix=''):
-        lines = []
-        name_pattern = indent + '    ' + prefix + '**{0}** '
-        if not is_empty(self.desc[0]):
-            name_pattern += '- '
-        name = name_pattern.format(self.name)
-        paragraph = ' '.join(remove_indent(self.desc))
-        lines.append(name + paragraph)
-        return lines
-
+from base_doc import BaseDoc
+from util import get_indent, add_indent
+from fields import ArgumentField, ListItemWithTypeField, ListItemField
 
 
 class FunctionDoc(BaseDoc):
@@ -51,42 +24,19 @@ class FunctionDoc(BaseDoc):
         return
 
     def _refactor_returns(self, header):
-        """Refactor the return section to sphinx friendly format"""
+        """Refactor the return section to sphinx friendly format.
 
-        if self.verbose:
-            print 'Returns section refactoring'
-
-        descriptions = []
+        """
         index = self.index
         self.remove_lines(index, 2)
         indent = get_indent(self.peek())
-        fields = self.extract_fields(indent)
-
-        if self.verbose:
-            print 'Return items'
-            print fields
-
-        # generate sphinx friendly rst
-        descriptions = []
-        descriptions.append(indent + ':returns:')
-        if len(fields) == 1:
-            name_format = '**{0}** '
-        else:
-            name_format = '- **{0}** '
-
-        for arg_name, arg_type, desc in fields:
-            arg_name = indent + '    ' + name_format.format(arg_name)
-            if arg_type != '':
-                arg_type = '({0})'.format(arg_type)
-            else:
-                arg_type = ''
-            if not is_empty(desc[0]):
-                arg_type = arg_type + ' - '
-            paragraph = ' '.join(remove_indent(desc))
-            descriptions.append(arg_name + arg_type + paragraph)
-
-        self.insert_lines(descriptions, index)
-        self.index += len(descriptions)
+        fields = self.extract_fields(indent, field_type=ListItemWithTypeField)
+        lines = [indent + ':returns:']
+        prefix = '' if len(fields) == 1 else '- '
+        for field in fields:
+            lines += field.to_rst(len(indent) + 4, prefix)
+        self.insert_lines(lines, index)
+        self.index += len(lines)
         return
 
     def _refactor_raises(self, header):
@@ -94,13 +44,11 @@ class FunctionDoc(BaseDoc):
         index = self.index
         self.remove_lines(index, 2)
         indent = get_indent(self.peek())
-        fields = self.extract_fields(indent)
-        lines = []
-        lines.append(indent + ':raises:')
+        fields = self.extract_fields(indent, field_type=ListItemField)
+        lines = [indent + ':raises:']
         prefix = '' if len(fields) == 1 else '- '
         for field in fields:
-            raises_field = RaisesField._make(field)
-            lines += raises_field.rst(indent, prefix)
+            lines += field.to_rst(len(indent) + 4, prefix)
         self.insert_lines(lines, index)
         self.index += len(lines)
         return
@@ -111,13 +59,10 @@ class FunctionDoc(BaseDoc):
         index = self.index
         self.remove_lines(index, 2)
         indent = get_indent(self.peek())
-        fields = self.extract_fields(indent)
-
+        fields = self.extract_fields(indent, field_type=ArgumentField)
         lines = []
         for field in fields:
-            argument_field = ArgumentField._make(field)
-            lines += argument_field.rst(indent)
-
+            lines += field.to_rst(len(indent))
         self.insert_lines(lines, index)
         self.index += len(lines)
         return

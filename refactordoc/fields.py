ab00688ff3cb26a27179c0a909305bee1459a3c4
itziakos <ioannist@enthought.com>
2012-01-19 10:08:07 +0000

    RFC: Rename and cleanup Field Class
    - Rename Field to DefinitionItem to follow the rst markup description.
    - Update class docstrings
    - Cleanup to_rst method
    - Precompile regexes
    - Simply method signatures (remove `indent` argument)
    
    Todo:
    - Cleanup parse classmethod

--- refactordoc/fields.py
+++ refactordoc/fields.py
@@ -9,112 +9,155 @@
 import collections
 import re
 
-from line_functions import add_indent, is_empty, remove_indent, replace_at
+from line_functions import add_indent, is_empty, remove_indent, replace_at, insert_empty
 
 
-class Field(collections.namedtuple('Field', ('name','signature','desc'))):
-    """ A docstring field.
+header_regex = re.compile('\s\:\s?')
+definition_regex = re.compile('\*?\*?\w+\s:(\s+|$)')
 
-    The class is based on the nametuple class and represents the logic
-    to check, parse and refactor a docstring field.
+class DefinitionItem(collections.namedtuple('Field', ('term','classifier','definition'))):
+    """ A docstring definition item
+
+    Syntax diagram::
+
+    +----------------------------+
+    | term [ " : " classifier ]* |
+    +--+-------------------------+--+
+       | definition                 |
+       | (body elements)+           |
+       +----------------------------+
+
+    A Definition item is the base item of a section definition list.
+    (please see _http://docutils.sourceforge.net/docs/ref/rst/restructuredtext.html#sections)
+
+    The Definition class is based on the nametuple class and is responsible
+    for to check, parse and refactor a docstring definition item into sphinx
+    friently rst.
 
     Attributes
     ----------
-    name : str
-        The name if the field, usssualy the name of a parameter of atrribute.
+    term : str
+        The term ussualy relects the name of a parameter or an attribute.
 
-    signature : str
-        The signature of the field. Commonly is the class type of an argument
-        or the signature of a function.
+    classifier: str
+        The classifier of the defintion. Commonly used to reflect the type
+        of an argument or the signature of a function.
 
-    desc : str
-        The description of the field. Given the type of the field this is a
-        single paragraph or a block of rst source.
+        .. note:: Currently only one classifier is supported.
+
+    definition : str
+        The description of the definition item.
 
     """
 
     @classmethod
-    def is_field(cls, line, indent=''):
-        """ Check if the line is a field header.
+    def is_definition(cls, line):
+        """ Check if the line is describing a definition item.
+
+        The method is used to check that a line is following the expected
+        format for the term and classifier attributes.
+
+        The expected format is::
+
+        +----------------------------+
+        | term [ " : " classifier ]  |
+        +----------------------------+
+
+        Subclasses can subclass to restict or expand this format.
+
         """
-        regex = indent + r'\*?\*?\w+\s:(\s+|$)'
-        match = re.match(regex, line)
-        return match
+        return definition_regex.match(line)
 
     @classmethod
     def parse(cls, lines):
-        """Parse a field definition for a set of lines.
+        """Parse a definition item from a set of lines.
 
         The field is assumed to be in one of the following formats::
 
-            <name> : <type>
-                <description>
+        term
+            Definition.
 
-        or::
+        ::
 
-            <name> :
-                <description>
+        term
+            Definition, paragraph 1.
 
-        or::
+            Definition, paragraph 2.
 
-            <name>
-                <description>
+        ::
+
+        term : classifier
+            Definition.
 
         Arguments
         ---------
         lines :
-            docstring lines of the field without any empty lines before or
+            docstring lines of the definition without any empty lines before or
             after.
 
         Returns
         -------
-        field :
-            Field or subclass of Field
+        definition : Definition
 
         """
         header = lines[0].strip()
         if ' :' in header:
-            arg_name, arg_type = re.split('\s\:\s?', header, maxsplit=1)
+            term, classifier = header_regex.split(header, maxsplit=1)
         else:
-            arg_name, arg_type = header, ''
+            arg_name, signature = header, ''
         if len(lines) > 1:
             lines = [line.rstrip() for line in lines]
-            return cls(arg_name.strip(), arg_type.strip(), lines[1:])
+            return cls(arg_name.strip(), signature.strip(), lines[1:])
         else:
-            return cls(arg_name.strip(), arg_type.strip(), [''])
+            return cls(arg_name.strip(), signature.strip(), [''])
 
 
-    def to_rst(self, indent=4):
-        """ Outputs field in rst as an itme in a definition list.
+    def to_rst(self):
+        """ Outputs the Definition in sphinx friendly rst.
+
+        The method renders the definition into a list of lines that follow
+        the rst markup. The default behaviour is to render the definition
+        as an sphinx definition item::
+
+        <term>
+
+           (<classifier>) --
+           <definition>
+
+        Subclasses will ussualy override the method to provide custom made
+        behaviour.
 
         Arguments
         ---------
         indent : int
-            The indent to use for the decription block.
-
+            The indent to use for the decription block. Default is 4 spaces
 
         Returns
         -------
         lines : list
-            A list of string lines of formated rst.
+            A list of string lines rendered in rst.
 
         Example
         -------
 
         >>> Field('Ioannis', 'Ιωάννης', 'Is the greek guy.')
         >>> print Field.to_rst()
-        Ioannis (Ιωάννης)
+        Ioannis
+
+            *(Ιωάννης)* --
             Is the greek guy.
 
         """
         lines = []
-        header = '{0} ({1})'.format(self.name, self.signature)
-        lines.append(header)
-        lines += add_indent(self.desc, indent)
+        lines += [self.term]
+        iines += ['*({0})*'.format(self.classifier)]
+        lines += [self.definition]
         return lines
 
+
+
 class AttributeField(Field):
-    """ Field for the argument function docstrings """
+    """ Field that renders the rst output as an attribute """
 
     def to_rst(self, indent=4):
         """ Outputs field in rst using the ``:param:`` role.
@@ -129,7 +172,7 @@ class AttributeField(Field):
 
         >>> Field('indent', 'int', 'The indent to use for the decription block.')
         >>> print Field.to_rst()
-        :param indent: The indent to use for the description block
+        The indent to use for the description block
         :type indent: int
 
         """

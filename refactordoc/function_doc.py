6b84635b0893e1d7eb766d2c9dbe1946c50ce069
itziakos <ioannist@enthought.com>
2012-01-30 02:13:36 +0000

    Refactor FunctionDoc
    - use new DefinitionItem classes
    - merge _header_returns amd _header_raises into _refactor_field_list

--- refactordoc/function_doc.py
+++ refactordoc/function_doc.py
@@ -8,81 +8,50 @@
 #------------------------------------------------------------------------------
 from base_doc import BaseDoc
 from line_functions import get_indent, add_indent
-from fields import ArgumentField, ListItemWithTypeField, ListItemField
+from definition_items import ArgumentItem, ListItem
 
 
 class FunctionDoc(BaseDoc):
     """Docstring refactoring for functions"""
 
-    def __init__(self, lines, headers=None, verbose=False):
+    def __init__(self, lines, headers=None):
 
         if headers is None:
-            headers = {'Returns': 'returns', 'Arguments': 'arguments',
-                       'Parameters': 'arguments', 'Raises': 'raises',
-                       'Yields': 'returns', 'Notes':'notes'}
+            headers = {'Returns': 'field_list', 'Arguments': 'arguments',
+                       'Parameters': 'arguments', 'Raises': 'field_list',
+                       'Yields': 'field_list', 'Notes':'notes'}
 
-        super(FunctionDoc, self).__init__(lines, headers, verbose)
+        super(FunctionDoc, self).__init__(lines, headers)
         return
 
-    def _refactor_returns(self, header):
-        """Refactor the return section to sphinx friendly format.
+    def _refactor_field_list(self, header):
+        """Refactor the a section to sphinx friendly item list.
 
         """
-        index = self.index
-        self.remove_lines(index, 2)
-        indent = get_indent(self.peek())
-        fields = self.extract_fields(indent, field_type=ListItemWithTypeField)
-        lines = [indent + ':returns:']
-        prefix = '' if len(fields) == 1 else '- '
-        for field in fields:
-            lines += field.to_rst(len(indent) + 4, prefix)
-        self.insert_lines(lines, index)
-        self.index += len(lines)
-        return
-
-    def _refactor_raises(self, header):
-        """Refactor the raises section to sphinx friendly format"""
-        index = self.index
-        self.remove_lines(index, 2)
-        indent = get_indent(self.peek())
-        fields = self.extract_fields(indent, field_type=ListItemField)
-        lines = [indent + ':raises:']
-        prefix = '' if len(fields) == 1 else '- '
-        for field in fields:
-            lines += field.to_rst(len(indent) + 4, prefix)
-        self.insert_lines(lines, index)
-        self.index += len(lines)
+        items = self.extract_items(item_class=ListItem)
+        lines = [':{0}:'.format(header.lower())]
+        prefix = None if len(items) == 1 else '-'
+        for item in items:
+            lines += add_indent(item.to_rst(prefix))
+        self.insert_and_move(lines, self.index)
         return
 
     def _refactor_arguments(self, header):
         """Refactor the argument section to sphinx friendly format
         """
-        index = self.index
-        self.remove_lines(index, 2)
-        indent = get_indent(self.peek())
-        fields = self.extract_fields(indent, field_type=ArgumentField)
+        items = self.extract_items(item_class=ArgumentItem)
         lines = []
-        for field in fields:
-            lines += field.to_rst(len(indent))
-        self.insert_lines(lines, index)
-        self.index += len(lines)
+        for item in items:
+            lines += item.to_rst()
+        self.insert_and_move(lines, self.index)
         return
 
     def _refactor_notes(self, header):
-        """Refactor the argument section to sphinx friendly format.
+        """Refactor the notes section to sphinx friendly format.
 
         """
-        if self.verbose:
-            print 'Refactoring Notes'
-
-        descriptions = []
-        index = self.index
-        self.remove_lines(index, 2)
-        indent = get_indent(self.peek())
         paragraph = self.get_next_paragraph()
-        descriptions.append(indent + '.. note::')
-        descriptions += add_indent(paragraph)
-        self.insert_lines(descriptions, index)
-        self.index += len(descriptions)
-        return descriptions
+        lines = ['.. note::']
+        lines += add_indent(paragraph)
+        self.insert_and_move(lines, self.index)
 

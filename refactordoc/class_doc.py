c9d55beea1d4370abec1827662eebdc59be17e3c
itziakos <ioannist@enthought.com>
2012-01-30 02:26:51 +0000

    Refactor, Simplify and Cleanup ClassDoc

--- refactordoc/class_doc.py
+++ refactordoc/class_doc.py
@@ -8,95 +8,85 @@
 #------------------------------------------------------------------------------
 from base_doc import BaseDoc
 from line_functions import get_indent, replace_at, add_indent
-from fields import (max_header_length, max_desc_length,
-                    max_name_length, MethodField, AttributeField)
+from definition_items import (MethodItem, AttributeItem, TableLineItem,
+                              max_attribute_length, max_attribute_index)
 
 
 class ClassDoc(BaseDoc):
     """Docstring refactoring for classes"""
 
-    def __init__(self, lines, headers=None, verbose=False):
+    def __init__(self, lines, headers=None):
 
         if headers is None:
             headers = {'Attributes': 'attributes', 'Methods': 'methods',
-                       'See Also': 'header', 'Abstract Methods': 'methods',
                        'Notes':'notes'}
 
-        super(ClassDoc, self).__init__(lines, headers, verbose)
+        super(ClassDoc, self).__init__(lines, headers)
         return
 
     def _refactor_attributes(self, header):
         """Refactor the attributes section to sphinx friendly format"""
-
-        if self.verbose:
-            print '{0} Section'.format(header)
-
-        index = self.index
-        self.remove_lines(index, 2)
-        indent = get_indent(self.peek())
-        fields = self.extract_fields(indent, AttributeField)
-
-        descriptions = []
-        for field in fields:
-            descriptions += field.to_rst(len(indent))
-        self.insert_lines(descriptions[:-1], index)
-        self.index += len(descriptions)
+        items = self.extract_items(AttributeItem)
+        lines = []
+        for item in items:
+            lines += item.to_rst()
+        self.insert_and_move(lines, self.index)
         return
 
     def _refactor_methods(self, header):
         """Refactor the methods section to sphinx friendly format.
 
         """
-        if self.verbose:
-            print '{0} section'.format(header)
-
-        index = self.index
-        self.remove_lines(index, 2)
-        indent = get_indent(self.peek())
-        method_fields = self.extract_fields(indent, MethodField)
+        items = self.extract_items(MethodItem)
 
         lines = []
-        if len(method_fields) > 0 :
-            name_length = max_name_length(method_fields)
-            method_length = max_header_length(method_fields)
-            desc_length = max_desc_length(method_fields)
-
-            first_column = len(indent)
-            second_column = first_column + method_length + name_length + 8
-            first_column_str = '=' * (method_length + name_length + 7)
-            second_column_str = '=' * desc_length
-
-            border = '{0}{1} {2}'.format(indent,
-                                              first_column_str,
-                                              second_column_str)
-            length = len(border)
-            empty = length * ' '
-            headings = empty[:]
-            headings = replace_at('Methods', headings, first_column)
-            headings = replace_at('Description', headings, second_column)
-            lines.append(border)
-            lines.append(headings)
-            lines.append(border)
-            for field in method_fields:
-                lines += field.to_rst(length, first_column, second_column)
-            lines.append(border)
-
+        if len(items) > 0 :
+            columns = self._get_column_lengths(items)
+            border = '{0:=^{1}} {0:=^{2}}'.format('', columns[0], columns[1])
+            heading = '{0:<{2}} {1:<{3}}'.format('Method', 'Description',
+                                                 columns[0], columns[1])
+            lines += [border]
+            lines += [heading]
+            lines += [border]
+            for items in items:
+                lines += items.to_rst(columns)
+            lines += [border]
+            lines += ['']
         lines = [line.rstrip() for line in lines]
-        self.insert_lines(lines, index)
-        self.index += len(lines)
+        self.insert_and_move(lines, self.index)
         return
 
     def _refactor_notes(self, header):
         """Refactor the note section to use the rst ``.. note`` directive.
 
         """
-        descriptions = []
-        index = self.index
-        self.remove_lines(index, 2)
-        indent = get_indent(self.peek())
         paragraph = self.get_next_paragraph()
-        descriptions.append(indent + '.. note::')
-        descriptions += add_indent(paragraph)
-        self.insert_lines(descriptions, index)
-        self.index += len(descriptions)
-        return descriptions
+        lines = ['.. note::']
+        lines += add_indent(paragraph)
+        self.insert_and_move(lines, self.index)
+
+    def _get_column_lengths(self, items):
+        """ Helper function to estimate the column widths for the refactoring of
+        the ``Methods`` section.
+
+        The method finds the index of the item that has the largest function
+        name (i.e. self.term) and the largest signature. If the indexes are not
+        the same then checks to see which of the two items have the largest
+        string sum (i.e. self.term + self.signature).
+
+        """
+        name_index = max_attribute_index(items, 'term')
+        signature_index = max_attribute_index(items, 'signature')
+        if signature_index != name_index:
+            index = signature_index
+            item1_width = len(items[index].term + items[index].signature)
+            index = name_index
+            item2_width = len(items[index].term + items[index].signature)
+            first_column = max(item1_width, item2_width)
+        else:
+            index = name_index
+            first_column = len(items[index].term + items[index].signature)
+
+        first_column += 11  # Add boilerplate characters
+        second_column = max_attribute_length(items, 'definition')
+        return (first_column, second_column)

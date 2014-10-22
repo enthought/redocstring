f7e9b05eef263a72c3dc333dba63cecd2db64f00
Ioannis Tziakos <ioannist@enthought.com>
2014-10-22 02:27:32 +0100

    use the default role for the classifiers

--- refactordoc/renderers/list_item.py
+++ refactordoc/renderers/list_item.py
@@ -8,9 +8,9 @@ class ListItem(Renderer):
     """
 
     templates = {
-        "full": "**{0}** (*{1}*) --\n{2}\n\n",
+        "full": "**{0}** (`{1}`) --\n{2}\n\n",
         "only_term": "**{0}**\n\n",
-        "no_definition": "**{0}** (*{1}*)\n\n",
+        "no_definition": "**{0}** (`{1}`)\n\n",
         "no_classifiers": "**{0}** --\n{2}\n\n"}
 
     def to_rst(self, prefix=None):

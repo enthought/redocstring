bd5add51fe6b2ffec44c09325011c2e6594ea703
itziakos <mail@itziakos.gr>
2014-11-03 22:39:53 +0000

    remove autogenerated file

--- redocstring/_version.py
+++
@@ -1,8 +0,0 @@
-# THIS FILE IS GENERATED FROM REDOCSTRING SETUP.PY
-version = '0.4.0'
-full_version = '0.4.0.dev1-552210b'
-git_revision = '552210bbf0c19b015b7a8b39c3982f0dc53e7c57'
-is_released = False
-
-if not is_released:
-    version = full_version

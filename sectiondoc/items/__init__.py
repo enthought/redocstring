f7b83437032a180a2f3957fa53847325907d0268
Ioannis Tziakos <ioannist@enthought.com>
2015-02-11 20:25:14 +0000

    Update __init__.py

--- sectiondoc/items/__init__.py
+++ sectiondoc/items/__init__.py
@@ -4,5 +4,5 @@ __all__ = [
     'Item']
 
 from sectiondoc.items.item import Item
-from sectiondoc.items.definition_item import OrDefinitionItem
+from sectiondoc.items.or_definition_item import OrDefinitionItem
 from sectiondoc.items.method_item import MethodItem

0d6ab14d481e8d33ac044334420698a4238b66f9
Ioannis Tziakos <ioannist@enthought.com>
2015-02-27 21:44:30 +0000

    update docstring

--- sectiondoc/renderers/definition.py
+++ sectiondoc/renderers/definition.py
@@ -7,18 +7,18 @@ class Definition(Renderer):
     def to_rst(self, **kwards):
         """ Outputs the Item in sphinx friendly rst.
 
-        The method renders the definition into a list of lines that follow
-        the rst markup of a sphinx definition item::
+        The method renders the ``definition`` into a list of lines that 
+        follow the rst markup of a sphinx definition item::
 
             <term>
 
                (<classifier(s)>) --
                <definition>
 
-        Subclasses will usually override the method to provide custom made
-        behaviour. However the signature of the method should hold only
+        Subclasses will usually override the method to provide custom
+        behaviour. However, the signature of the method should hold only
         keyword arguments which have default values. The keyword arguments
-        can be used to pass addition rendering information to subclasses.
+        can be used to pass additional rendering information to subclasses.
 
         Returns
         -------
